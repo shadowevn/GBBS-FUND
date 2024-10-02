b8f4db5ef4ade03f62496cbf73377c6f7ba6fc95
shadowevn <jeixiancen21@gmail.com>
2024-10-02 00:31:40 -0400

    completed first assingment

---
+++ Finished_Assignments/Assignment1.py
@@ -0,0 +1,32 @@
+# Evan Wu
+
+# Assignment 1: Variables
+
+# 1. Write a program that creates three variables: name, age, and height (you can use made up numbers). Then print them out.
+# 2. Imagine you have a friend who is a bit older than you, and you want to print out the age difference between you and your friend.
+# 3. You got married and changed your last name. Update the name variable to your new last name.
+# 4. You had a birthday! Update your age variable.
+
+# friend_name = "John"
+# friend_age = 30
+
+# name = "" #string
+
+# age = "" #integer
+
+# heigh = "" #float
+
+# print(name)
+# print(age)
+# print(heigh)
+
+# friend_age = 30
+# my_age = 25
+
+# print("Your age difference is", friend_age - my_age, "years")
+
+first_name = "John"
+current_last_name = input("what is your current last name?")
+new_last_name = input("What is your new last name?")
+
+print( "Your new name is:", first_name +" "+ new_last_name)
