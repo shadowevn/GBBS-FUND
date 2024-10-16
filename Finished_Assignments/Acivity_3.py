056626d753a01ac18f8d267d2cb30cee7995936a
shadowevn <jeixiancen21@gmail.com>
2024-10-15 22:18:07 -0400

    added ativity 3

---
+++ Finished_Assignments/Acivity_3.py
@@ -0,0 +1,69 @@
+import random
+
+# Activity 3:
+
+# Use forloops to solve the following problems.
+
+# This function returns a random number between 1 and 100
+# Usage: a = get_random_number()
+# a would be a random number between 1 and 100
+def get_random_number():
+    return random.randint(1, 100)
+
+
+# 1. Create a list of 10 random numbers between 1 and 100. Hint: Remeber how you can append(add) to a list.
+# Write your code below:
+
+
+
+
+# 2. Print the list of random numbers.
+# Write your code below:
+
+
+
+
+# 3. Print the sum of the list of random numbers.
+# Write your code below:
+
+
+
+
+# ---------------------------------------------------------
+
+# We will create a simple while loop calculator tool similar to what we did in lecture.
+
+# 1. First tell the user the commands they can use. The commands are: add, subtract, multiply, divide, exit
+
+# 2. Ask the user to enter a command. If the user enters "exit", stop the while loop and print "Goodbye".
+
+# 3. If the user enters "add", ask the user again for the first number, and then ask again for the second number. add them together and print the result.
+
+# 4. If the user enters "subtract", ask the user again for the first number, and then ask again for the second number. subtract the second number from the first number and print the result.
+
+# 5. If the user enters "multiply", ask the user again for the first number, and then ask again for the second number. multiply them together and print the result.
+
+# 6. If the user enters "divide", ask the user again for the first number, and then ask again for the second number. divide the first number by the second number and print the result.
+
+# Example 1:
+# Enter a command: add
+# Enter the first number: 5
+# Enter the second number: 3
+# The result is 8
+# Enter a command: exit
+
+# Example 2:
+# Enter a command: divide
+# Enter the first number: 10
+# Enter the second number: 2
+# The result is 5
+
+# Write your code below:
+
+
+
+
+
+
+
+
